98da35e651e257b6f4e88fad1a84132c01304e07
alexicm <87048425+alexicm@users.noreply.github.com>
2025-01-22 13:44:18 -0300

    deploy v1.2.2: security

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
+import bcrypt from "bcryptjs"
 
 type LoginPageProps = {}
 
